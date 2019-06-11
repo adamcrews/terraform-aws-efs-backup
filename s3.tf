990419516d886f50fe82627f448bf75b7a1bceda
Adam Crews <adam.crews@gmail.com>
2019-06-10 19:14:02 -0700

    Add s3 encryption, adjust lifecycle prefix

--- s3.tf
+++ s3.tf
@@ -1,3 +1,15 @@
+resource "aws_kms_key" "default" {
+  description             = "${module.backups_label.id}"
+  deletion_window_in_days = "30"
+  enable_key_rotation     = "true"
+  tags                    = "${module.backups_label.tags}"
+}
+
+resource "aws_kms_alias" "default" {
+  name          = "${format("alias/%v", module.backups_label.id)}"
+  target_key_id = "${aws_kms_key.default.id}"
+}
+
 module "logs_label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
   namespace  = "${var.namespace}"
@@ -12,6 +24,15 @@ resource "aws_s3_bucket" "logs" {
   bucket        = "${module.logs_label.id}"
   force_destroy = true
   tags          = "${module.logs_label.tags}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.default.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 module "backups_label" {
@@ -32,12 +53,36 @@ resource "aws_s3_bucket" "backups" {
     enabled = true
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.default.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
   lifecycle_rule {
     enabled = true
-    prefix  = "efs"
 
     noncurrent_version_expiration {
       days = "${var.noncurrent_version_expiration_days}"
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "backups" {
+  bucket                  = "${aws_s3_bucket.backups.id}"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket                  = "${aws_s3_bucket.logs.id}"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
